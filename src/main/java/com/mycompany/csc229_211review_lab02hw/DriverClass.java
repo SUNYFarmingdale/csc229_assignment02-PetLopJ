faa5f5a215b773d70d814d562e546f01d4962bc4
PetLopJ <Peterjlopez0911@gmail.com>
2024-02-10 11:48:22 -0500

    commit
    Student.java
    Students is child of Person
    Adds gpa with getters and setters
    Adds setters and getters for address
    Adds toString method
    
    DriverClass.java
    Imports Scanner
    Sets GPA with input
    Uses toString to output results

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -4,21 +4,28 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
+import java.util.Scanner;
 public class DriverClass {
 	
 	public static void main(String[] args) {
 		// ToDo 5: Fix the error
-		
+
 		// ToDo 6: Fix the constructor of Student class
-		
+
 		// ToDo 7: Add a toString method for Student class
-		
-		Student std1= new Student("James", 20);
+
+		Student std1= new Student("James", (short) 20);
 		
 		// ToDo 8: Set the gpa of the student using the scanner and user
 		// 			input and then print the output.
-		
-		System.out.println(std1);
+		// Uses scanner to set GPA for std1
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter GPA for " + std1.getName() + ": ");
+		double gpaInput = scanner.nextDouble();
+		std1.setGpa(gpaInput);
+
+		// Uses the toString method to print name, age, and GPA
+		System.out.println(std1.toString());
 		
 		// ToDo 9: add comments and explain your code
 		
--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,13 +4,47 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
+    //Makes Student class a child of Person
+public class Student extends Person {
+
+    // ToDo 1: Make this class a child of Person
 	// ToDo 2: Fix the resulting errors
-	
+
 	// ToDo 3: Add a field for GPA and create setter and getter
-	
+    //initializes GPA to 0.0
+    private double gpa = 0.0;
+
+    // Constructor for Student
+    public Student(String name, short age) {
+        super(name, age);
+
+    }
+
+    // setter for GPA
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    // getter for GPA
+    public double getGpa(){
+        return gpa;
+    }
+
+    // setter that inherits from Person class
+    public void setAddress(String address){
+
+    }
+    // getter that inherits from Person class
+    public String getAddress(){
+        return null;
+    }
+    // method that prints name, age, and gpa of student
+    public String toString() {
+        return "Student Name: " + getName() + ", Age: " + getAge() + ", GPA: " + getGpa();
+    }
+
+
 	// ToDo 4: Add comments to your code
+
 
 }
